3b23fc3f754555afd8e62f957c27a84c9e406208
JS Chai <chunsaa@gmail.com>
2020-03-24 02:06:29 -0400

    change due to source csv format update. also added new confirmed and deaths columns

--- src/App.js
+++ src/App.js
@@ -17,81 +17,106 @@ function App() {
     Search: forwardRef((props, ref) => <Search {...props} ref={ref} />)
   };
 
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({
+    confirmed: null,
+    deaths: null,
+    latestDate: null,
+    prevDate: null,
+    data: null
+  })
 
   useEffect(() => {
-    const today = getDate();
-    axios.get(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/${today}.csv`)
-      .then(res => {
-        const parsedData = Papa.parse(res.data).data;
-        parsedData.shift()
-        const countries = {};
-        parsedData.forEach(elm => {
-          if (countries[elm[1]]) {
-            countries[elm[1]].confirmed += parseInt(elm[3]);
-            countries[elm[1]].deaths += parseInt(elm[4]);
-            countries[elm[1]].recovered += parseInt(elm[5]);
-          } else {
-            countries[elm[1]] = {
-              confirmed: parseInt(elm[3]),
-              deaths: parseInt(elm[4]),
-              recovered: parseInt(elm[5])
-            };
+    const confirmed = axios.get(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv`);
+    const deaths = axios.get(`https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv`);
+    Promise.all([
+      Promise.resolve(confirmed),
+      Promise.resolve(deaths)
+    ]).then(all => {
+      const confirmedParsed = Papa.parse(all[0].data).data;
+      const deathsParsed = Papa.parse(all[1].data).data;
+      setData({
+        ...data,
+        latestDate: confirmedParsed[0][confirmedParsed[0].length - 1],
+        prevDate: confirmedParsed[0][confirmedParsed[0].length - 2],
+        confirmed: confirmedParsed,
+        deaths: deathsParsed
+      })
+    })
+  }, [])
+
+  useEffect(() => {
+    if (data.confirmed && data.deaths && data.latestDate && data.prevDate) {
+      data.confirmed.shift();
+      data.deaths.shift();
+      const countries = {};
+      data.confirmed.forEach(elm => {
+        if (countries[elm[1]]) {
+          countries[elm[1]].confirmedLatest += parseInt(elm[elm.length - 1]);
+          countries[elm[1]].confirmedPrev += parseInt(elm[elm.length - 2]);
+        } else {
+          countries[elm[1]] = {
+            confirmedLatest: parseInt(elm[elm.length - 1]),
+            confirmedPrev: parseInt(elm[elm.length - 2]),
+            deathsLatest: 0,
+            deathsPrev: 0
           }
-        })
-        setData(countries);
+        }
       })
-      .catch(err => {
-        alert(err)
+      data.deaths.forEach(elm => {
+        if (countries[elm[1]]) {
+          countries[elm[1]].deathsLatest += parseInt(elm[elm.length - 1]);
+          countries[elm[1]].deathsPrev += parseInt(elm[elm.length - 2]);
+        } else {
+          countries[elm[1]] = {
+            confirmedLatest: 0,
+            confirmedPrev: 0,
+            deathsLatest: parseInt(elm[elm.length - 1]),
+            deathsPrev: parseInt(elm[elm.length - 2])
+          }
+        }
       })
-  }, []);
-
-  const getDate = () => {
-    let today = new Date();
-    today.setDate(today.getDate() - 1);
-    let dd = today.getDate();
-
-    let mm = today.getMonth() + 1;
-    const yyyy = today.getFullYear();
-    if (dd < 10) {
-      dd = `0${dd}`;
+      setData({
+        ...data,
+        data: countries
+      })
     }
-
-    if (mm < 10) {
-      mm = `0${mm}`;
-    }
-    today = mm + '-' + dd + '-' + yyyy;
-    return today;
-  }
+  }, [data.confirmed && data.deaths && data.latestDate && data.prevDate])
 
   const createColumn = () => {
     const columns = [
       { title: 'Country', field: 'country' },
-      { title: 'Confirmed', field: 'confirmed' },
-      { title: 'Deaths', field: 'deaths' },
-      { title: 'Recovered', field: 'recovered' }
+      { title: 'Confirmed', field: 'confirmedLatest' },
+      { title: 'New Confirmed', field: 'newConfirmed' },
+      { title: 'Deaths', field: 'deathsLatest' },
+      { title: 'New Deaths', field: 'newDeaths' }
     ]
     return columns;
   }
 
   const createData = () => {
     const dataArray = [];
-    if (data) {
-      for (const [country, value] of Object.entries(data)) {
-        const object = { country, confirmed: value.confirmed, deaths: value.deaths, recovered: value.recovered };
+    if (data.data) {
+      for (const [country, value] of Object.entries(data.data)) {
+        const object = {
+          country,
+          confirmedLatest: value.confirmedLatest,
+          newConfirmed: value.confirmedLatest - value.confirmedPrev,
+          deathsLatest: value.deathsLatest,
+          newDeaths: value.deathsLatest - value.deathsPrev
+        }
         dataArray.push(object);
       }
     }
-    dataArray.sort((a, b) => b.confirmed - a.confirmed)
+    dataArray.sort((a, b) => b.confirmedLatest - a.confirmedLatest)
     return dataArray;
   }
-
+  
   const buildTable = () => {
     if (data) {
       return (
         <MaterialTable
           icons={tableIcons}
-          title={`COVID-19 Global Stats Summary Ending ${getDate()}`}
+          title={`COVID-19 Global Stats Summary Ending ${data.latestDate}`}
           options={{ sorting: false, draggable: false, pageSize: 20, pageSizeOptions: [] }}
           columns={createColumn()}
           data={createData()}
